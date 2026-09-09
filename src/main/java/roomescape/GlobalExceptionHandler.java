2cf165a4e7a3b0a8084c8cabfebe0015ce44b731
se0ye2n <hsy040925@gmail.com>
2026-09-09 12:30:24 +0900

    fix: [9단계] 잘못된 시간 요청과 참조 중인 시간 삭제 예외 처리

--- src/main/java/roomescape/GlobalExceptionHandler.java
+++ src/main/java/roomescape/GlobalExceptionHandler.java
@@ -5,6 +5,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,7 +22,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler({
             MethodArgumentNotValidException.class,
-            InvalidReservationException.class
+            InvalidReservationException.class,
+            HttpMessageNotReadableException.class
     })
     public ResponseEntity<ErrorResponse> handleInvalidRequest() {
         return ResponseEntity
@@ -35,5 +38,12 @@ public class GlobalExceptionHandler {
         return ResponseEntity
                 .status(HttpStatus.NOT_FOUND)
                 .body(new ErrorResponse(exception.getMessage()));
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<ErrorResponse> handleDataIntegrityViolation() {
+        return ResponseEntity
+                .status(HttpStatus.CONFLICT)
+                .body(new ErrorResponse("다른 데이터에서 사용 중이거나 참조가 올바르지 않습니다."));
     }
 }
